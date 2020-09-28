39729509ce9b27bf7a15352319b5aed82c522e69
Slimakanzer <gleb.larochkin@gmail.com>
2020-09-28 08:34:10 +0300

    [software testing] use equivalence analysis to test function

--- software-testing/lab1/src/test/scala/ArctgFunctionTest.java
+++ software-testing/lab1/src/test/scala/ArctgFunctionTest.java
@@ -2,31 +2,52 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ArctgFunctionTest {
+
     @Test
-    public void testRightBorder(){
-        Assert.assertEquals("Right border of arctg (45°)", Math.PI / 4, MyMath.arctg(1.0), MyMath.Precision());
+    public void testFirstRegionRightBorder(){
+        Assert.assertEquals(0.46364760900, MyMath.arctg(0.5), MyMath.Precision());
     }
 
     @Test
-    public void testLeftBorder(){
-        Assert.assertEquals("Left border of arctg (-45°)", -Math.PI / 4, MyMath.arctg(-1.0), MyMath.Precision());
-    }
-
-    @Test
-    public void testZeroValue(){
-        Assert.assertEquals("Zero value of arctg (intersection with OX)", 0, MyMath.arctg(0), MyMath.Precision());
-    }
-
-    @Test
-    public void testPositiveXValues(){
-        Assert.assertEquals("arctg with 30°", Math.PI / 6, MyMath.arctg(1 / Math.sqrt(3)), MyMath.Precision());
+    public void testFirstRegionPositiveValue(){
+        Assert.assertEquals(0.24497866312, MyMath.arctg(0.25), MyMath.Precision());
         Assert.assertEquals("arctg with 15°", Math.PI / 6 / 2, MyMath.arctg(2 - Math.sqrt(3)), MyMath.Precision());
     }
 
     @Test
-    public void testNegativeXValues(){
-        Assert.assertEquals("arctg with -30°", -Math.PI / 6, MyMath.arctg(-1 / Math.sqrt(3)), MyMath.Precision());
+    public void testFirstRegionZeroValue(){
+        Assert.assertEquals("Zero value of arctg (intersection with OX)", 0, MyMath.arctg(0), MyMath.Precision());
+    }
+
+    @Test
+    public void testFirstRegionNegativeValue(){
+        Assert.assertEquals(-0.24497866312, MyMath.arctg(-0.25), MyMath.Precision());
         Assert.assertEquals("arctg with -15°", -Math.PI / 6 / 2, MyMath.arctg(-2 + Math.sqrt(3)), MyMath.Precision());
+    }
+
+    @Test
+    public void testFirstRegionLeftBorder(){
+        Assert.assertEquals(-0.46364760900, MyMath.arctg(-0.5), MyMath.Precision());
+    }
+
+    @Test
+    public void testSecondRegionValue(){
+        Assert.assertEquals("arctg with 30°", Math.PI / 6, MyMath.arctg(1 / Math.sqrt(3)), MyMath.Precision());
+    }
+
+    @Test
+    public void testSecondRegionRightBorder(){
+        Assert.assertEquals("Right border of arctg (45°)", Math.PI / 4, MyMath.arctg(1.0), MyMath.Precision());
+    }
+
+    @Test
+    public void testThirdRegionValue(){
+        Assert.assertEquals("arctg with -30°", -Math.PI / 6, MyMath.arctg(-1 / Math.sqrt(3)), MyMath.Precision());
+    }
+
+    @Test
+    public void testThirdRegionLeftBorder(){
+        Assert.assertEquals("Left border of arctg (-45°)", -Math.PI / 4, MyMath.arctg(-1.0), MyMath.Precision());
     }
 
     @Test(expected = IllegalArgumentException.class)
