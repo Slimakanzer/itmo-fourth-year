01328e6807de85b8110dbe74c6dbbaec0f84b062
Slimakanzer <gleb.larochkin@gmail.com>
2020-11-19 23:15:07 +0300

    reduce the size of repo

--- software-testing/lab3/src/test/java/SeleniumTest.java
+++ software-testing/lab3/src/test/java/SeleniumTest.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -18,6 +20,8 @@ public class SeleniumTest {
     protected static final int TIMEOUT = 5;
     protected static final String URL = "https://www.meetup.com/";
     protected static final String SEARCH_URL = "https://www.meetup.com/find/?keywords=.net";
+    protected static final String CHROME_DRIVER_PATH = "src/test/resources/drivers/chromedriver.exe";
+    protected static final String FIREFOX_DRIVER_PATH = "src/test/resources/drivers/geckodriver.exe";
     protected RemoteWebDriver driver;
 
     @Parameterized.Parameters
@@ -30,8 +34,17 @@ public class SeleniumTest {
 
     @BeforeClass
     public static void setupEnvironments(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
+        var chromedriver = new File(CHROME_DRIVER_PATH).isFile();
+        var firefoxdriver = new File(FIREFOX_DRIVER_PATH).isFile();
+        if (!chromedriver.exists()) {
+            throw new FileNotFoundException("Can't find chrome driver. Please download and move it to the src/resources directory: https://chromedriver.chromium.org/downloads");
+        }
+        if (!firefoxdriver.exists()) {
+            throw new FileNotFoundException("Can't find firefox driver. Please download and move it to the src/resources directory: https://github.com/mozilla/geckodriver/releases");
+        }
+
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+        System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_PATH);
     }
 
     @Before
